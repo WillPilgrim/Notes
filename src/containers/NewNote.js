9330d62e67b150cb6d33d8f0a809ffcfac5a103e
WillPilgrim <sean.turner@iinet.net.au>
2018-06-10 22:44:00 +0800

    Build basic home component with authenticated switch

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -4,6 +4,7 @@ import LoaderButton from "../components/LoaderButton";
 import config from "../config";
 import "./NewNote.css";
 import { API } from "aws-amplify";
+import { s3Upload } from "../libs/awsLib";
 
 export default class NewNote extends Component {
   constructor(props) {
@@ -42,14 +43,19 @@ export default class NewNote extends Component {
     this.setState({ isLoading: true });
 
     try {
-        await this.createNote({
-          content: this.state.content
-        });
-        this.props.history.push("/");
-      } catch (e) {
-        alert(e);
-        this.setState({ isLoading: false });
-      }
+      const attachment = this.file
+        ? await s3Upload(this.file)
+        : null;
+  
+      await this.createNote({
+        attachment,
+        content: this.state.content
+      });
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e);
+      this.setState({ isLoading: false });
+    }
   }
 
   createNote(note) {
